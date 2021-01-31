711073979734dc95b4d2f6d0ac89a9f7058c7954
sarbagyastha <sarbagyastha@gmail.com>
2021-01-31 18:09:00 +0545

    added docs

--- example/lib/routes.dart
+++ example/lib/routes.dart
@@ -2,10 +2,14 @@ import 'package:clean_framework_example/example_feature/ui/example_feature_widge
 import 'package:clean_framework_example/payment/ui/payment_feature_widget.dart';
 import 'package:flutter/material.dart';
 
+///
 class Routes {
+  ///
   static const String example = '/';
+  ///
   static const String payment = '/payment';
 
+  ///
   static Widget generate(String routeName) {
     switch (routeName) {
       case example:
