ee5ab723ed85ce349123fa2be673756d352fa442
sarbagyastha <sarbagyastha@gmail.com>
2021-01-31 16:58:51 +0545

    added dart doc for `rest_api.dart`

--- lib/src/service/rest_api.dart
+++ lib/src/service/rest_api.dart
@@ -1,10 +1,45 @@
 import 'package:clean_framework/clean_framework.dart';
-import 'package:flutter/widgets.dart';
+import 'package:meta/meta.dart';
 
-enum RestMethod { get, post, put, delete, patch }
+/// The HTTP methods.
+enum RestMethod {
+  /// GET method
+  get,
 
-enum RestContentType { json, html, binary, text, unknown }
+  /// POST method
+  post,
 
+  /// PUT method
+  put,
+
+  /// DELETE method
+  delete,
+
+  /// PATCH method
+  patch,
+}
+
+/// The content type.
+enum RestContentType {
+  /// application/json
+  json,
+
+  /// text/html
+  html,
+
+  /// application/octet-stream
+  binary,
+
+  /// text/plain
+  text,
+
+  /// unknown
+  unknown,
+}
+
+/// A skeleton class for making RESTful API calls.
+///
+/// See [SimpleRestApi].
 abstract class RestApi<T extends RestResponse> extends ExternalDependency {
   /// The Uri for the request is built inside ResApi, to give flexibility on
   /// how it is used. The path parameter corresponds to the Uri path, which
@@ -15,31 +50,44 @@ abstract class RestApi<T extends RestResponse> extends ExternalDependency {
     Map<String, dynamic> requestBody = const {},
   });
 
+  /// Request for binary data.
   Future<T> requestBinary({
     @required RestMethod method,
     @required String path,
     Map<String, dynamic> requestBody = const {},
   });
 
+  /// Resolve to [RestResponseType] form status [code].
   RestResponseType getResponseTypeFromCode(int code) =>
       _responseCodeToRestResponseTypeMap[code] ?? RestResponseType.unknown;
 }
 
+/// The response signature for [RestApi].
 class RestResponse<T> {
+  /// The type of response. See [RestResponseType].
   final RestResponseType type;
+
+  /// The actual content of the reponse.
   final T content;
+
+  /// The uri to where the request was made.
   final Uri uri;
+
+  /// The content type. See [RestContentType].
   final RestContentType contentType;
-  RestResponse(
-      {this.type = RestResponseType.unknown,
-      this.content,
-      this.uri,
-      this.contentType = RestContentType.unknown})
-      : assert(content != null),
+
+  /// Creates a RestResponse.
+  RestResponse({
+    this.type = RestResponseType.unknown,
+    this.content,
+    this.uri,
+    this.contentType = RestContentType.unknown,
+  })  : assert(content != null),
         assert(uri != null);
 }
 
-final restContentTypeMap = {
+///
+final Map<String, RestContentType> restContentTypeMap = {
   'application/json; charset=utf-8': RestContentType.json,
   'text/html; charset=utf-8': RestContentType.html,
   'text/plain; charset=utf-8': RestContentType.text,
@@ -47,17 +95,31 @@ final restContentTypeMap = {
   'application/pdf': RestContentType.binary
 };
 
+/// The response type.
 enum RestResponseType {
-  // Success
+  /// Success
   success,
 
   // Errors
+  /// Time Out
   timeOut,
+
+  /// Bad Request
   badRequest,
+
+  /// 404 Not Found
   notFound,
+
+  /// Conflict
   conflict,
+
+  /// Internal Server Error
   internalServerError,
+
+  /// Unauthorized
   unauthorized,
+
+  /// Unknown
   unknown
 }
 
