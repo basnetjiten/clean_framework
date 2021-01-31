2249af55a229109de6af049a11e0f0e46e8fe040
sarbagyastha <sarbagyastha@gmail.com>
2021-01-31 19:45:45 +0545

    [null-safety] ConsoleLogger

--- lib/src/defaults/console_logger.dart
+++ lib/src/defaults/console_logger.dart
@@ -9,36 +9,36 @@ class ConsoleLogger implements Logger {
   void setLogLevel(LogLevel level) => this._level = level;
 
   @override
-  void fatal(message, [error, StackTrace stackTrace]) {
+  void fatal(message, [error, StackTrace? stackTrace]) {
     if (_level.index >= LogLevel.fatal.index)
       print('[FATAL]: $message ($error)');
   }
 
   @override
-  void error(message, [error, StackTrace stackTrace]) {
+  void error(message, [error, StackTrace? stackTrace]) {
     if (_level.index >= LogLevel.error.index)
       print('[ERROR]: $message ($error)');
   }
 
   @override
-  void warning(message, [error, StackTrace stackTrace]) {
+  void warning(message, [error, StackTrace? stackTrace]) {
     if (_level.index >= LogLevel.warning.index)
       print('[WARNING]: $message ($error)');
   }
 
   @override
-  void info(message, [error, StackTrace stackTrace]) {
+  void info(message, [error, StackTrace? stackTrace]) {
     if (_level.index >= LogLevel.info.index) print('[INFO]: $message ($error)');
   }
 
   @override
-  void debug(message, [error, StackTrace stackTrace]) {
+  void debug(message, [error, StackTrace? stackTrace]) {
     if (_level.index >= LogLevel.debug.index)
       print('[DEBUG]: $message ($error)');
   }
 
   @override
-  void verbose(message, [error, StackTrace stackTrace]) {
+  void verbose(message, [error, StackTrace? stackTrace]) {
     if (_level.index >= LogLevel.verbose.index)
       print('[VERBOSE]: $message ($error)');
   }
