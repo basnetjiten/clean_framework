691dd3bc81f543f0bac78bbfc438f027df0e0d00
sarbagyastha <sarbagyastha@gmail.com>
2021-02-14 17:49:59 +0545

    [null-safety] presenter

--- lib/src/ui/screen.dart
+++ lib/src/ui/screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 abstract class Screen extends StatelessWidget {
-  const Screen({Key key}) : super(key: key);
+  const Screen({Key? key}) : super(key: key);
 }
